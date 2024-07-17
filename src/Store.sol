6cf7f49f022d520d98c4dce6b7f4ea776b0a9506
Taras Woronjanski <tarasworonjanski@gmail.com>
2024-07-18 07:52:24 +1000

    ..

---
+++ src/Store.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MITs
+pragma solidity ^0.8.4;
+
+/*  Zk-kit Imports */
+import {InternalLeanIMT, LeanIMTData} from "zk-kit.solidity/packages/lean-imt/contracts/InternalLeanIMT.sol";
